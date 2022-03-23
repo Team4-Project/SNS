15e19a041c50768a242c25bd9f9dcd509674f43e
Noah <shgustmd6865@naver.com>
2022-03-23 12:53:42 +0900

    Feat: "댓글 좋아요 API 추가"
    댓글에 대해 좋아요 기능을 수행합니다.
    현재 세션 미구현으로 인해 userId=1로 고정되어 있습니다.
    PathVaribale로 commentId를 받아서 저장하고 있습니다.
    
    Fixes: #11

---
+++ src/main/java/com/team4/sns/controller/CommentLikeController.java
@@ -0,0 +1,22 @@
+package com.team4.sns.controller;
+
+import com.team4.sns.service.CommentLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentLikeController {
+
+    private final CommentLikeService commentLikeService;
+
+    @GetMapping(value = "/comment/like/{comment-id}")
+    public void likeComment(@PathVariable(name = "comment-id") Long commentId){
+
+        // 세션 미적용으로 인한 userId=1 고정
+        Long userId = 1L;
+        commentLikeService.likeComment(userId, commentId);
+    }
+}
---
+++ src/main/java/com/team4/sns/mapper/CommentLikeMapper.java
@@ -0,0 +1,11 @@
+package com.team4.sns.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface CommentLikeMapper {
+
+    void likeCommentByUserIdAndCommentId(@Param("userId") Long userId,
+                                         @Param("commentId") Long commentId);
+}
