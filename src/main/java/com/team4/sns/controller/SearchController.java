7e4d478bc6900d0d60855ecdbb5754b86993f774
김수연 <kimsuyeon@gimsuyeons-MacBook-Air.local>
2022-04-02 02:23:28 +0900

    키워드 검색시 피드보여주기 수정

--- src/main/java/com/team4/sns/controller/SearchController.java
+++ src/main/java/com/team4/sns/controller/SearchController.java
@@ -3,24 +3,25 @@ package com.team4.sns.controller;
 import com.team4.sns.service.PostService;
 import com.team4.sns.vo.Post;
 import com.team4.sns.vo.User;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class SearchController {
 
 	private PostService postService;
 
-	@GetMapping("/search")
-	public String searchByKeyword(Model model) {
-		List<Post> postList = postService.getPostByKeyword();
-		List<User> userList = postService.getUserByKeyword();
+	@GetMapping("/search/{keyword}")
+	public String searchByKeyword(Model model, @PathVariable String keyword) {
+		List<Post> postList = postService.getPostByKeyword(keyword);
 
-		model.addAttribute("users","userList");
-		model.addAttribute("posts","postList");
+		model.addAttribute("posts",postList);
 		return "search";
 	}
 }
