743f98ae9157c6eb308b8cdc6e1d9198473f333c
Noah <shgustmd6865@naver.com>
2022-04-29 01:16:49 +0900

    Tomcat 404 오류 해결을 위한 SpringBootServletInitializer 상속

--- src/main/java/com/team4/sns/SnsApplication.java
+++ src/main/java/com/team4/sns/SnsApplication.java
@@ -2,9 +2,16 @@ package com.team4.sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SnsApplication {
+public class SnsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SnsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnsApplication.class, args);
